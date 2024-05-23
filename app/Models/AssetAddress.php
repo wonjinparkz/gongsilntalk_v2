5c604e4ac7973c4bc4c5c3c09bbd18cd984fb578
ggangeunbee <ggang@rocateer.com>
2024-05-23 17:23:10 +0900

    [Model] 내 자산 모델 생성

---
+++ app/Models/AssetAddress.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class AssetAddress extends BaseModel
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * 테이블 명
+     */
+    protected $table = 'asset_address';
+
+    /**
+     * 모델
+     */
+    protected $fillable =
+    [
+        'users_id',
+        'is_temporary',
+        'is_unregistered',
+        'address_lat',
+        'address_lng',
+        'address',
+        'old_address'
+    ];
+
+
+    /**
+     * 직렬화에서 감출것
+     */
+    protected $hidden = [];
+
+    /**
+     * 캐스팅
+     */
+    protected $casts = [
+        'updated_at' => 'datetime',
+        'created_at' => 'datetime',
+    ];
+}
