07372c8874293e1f3e36832fd4583a52276e987f
준스 <junyeong@rocateer.com>
2024-06-05 16:34:14 +0900

    [관리자] 데이터관리 상가, 관리 모델 상속 수정

--- app/Models/DataBuilding.php
+++ app/Models/DataBuilding.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class DataBuilding extends Model
+class DataBuilding extends BaseModel
 {
     use HasFactory, Notifiable;
 
