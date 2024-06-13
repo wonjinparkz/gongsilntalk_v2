595fabe6766b077ab4b271586afbd89396370523
준스 <junyeong@rocateer.com>
2024-06-13 17:55:42 +0900

    [사용자] 바텀 메뉴 수정

--- resources/views/www/siteProduct/siteProduct_list.blade.php
+++ resources/views/www/siteProduct/siteProduct_list.blade.php
@@ -120,30 +120,7 @@
             <!-- paging : e -->
 
             <!-- nav : s -->
-            <nav>
-                <ul>
-                    <li>
-                        <a href="main.html"><span><img src="{{ asset('assets/media/mcnu_ic_1.png') }}"
-                                    alt=""></span>홈</a>
-                    </li>
-                    <li class="active">
-                        <a href="sales_list.html"><span><img src="{{ asset('assets/media/mcnu_ic_2.png') }}"
-                                    alt=""></span>분양현장</a>
-                    </li>
-                    <li>
-                        <a href="m_map.html"><span><img src="{{ asset('assets/media/mcnu_ic_3.png') }}"
-                                    alt=""></span>지도</a>
-                    </li>
-                    <li>
-                        <a href="community_contents_list.html"><span><img
-                                    src="{{ asset('assets/media/mcnu_ic_5.png') }}" alt=""></span>커뮤니티</a>
-                    </li>
-                    <li>
-                        <a href="my_main.html"><span><img src="{{ asset('assets/media/mcnu_ic_4.png') }}"
-                                    alt=""></span>마이페이지</a>
-                    </li>
-                </ul>
-            </nav>
+            <x-nav-layout />
             <!-- nav : e -->
 
 
