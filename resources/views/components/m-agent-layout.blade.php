2826c8652a794910a72b46297f0bd753343ba340
jsshim <jsshim@rocateer.com>
2024-06-19 14:05:17 +0900

    [사용자] 매물, 중개사무소 리스트 수정

--- resources/views/components/m-agent-layout.blade.php
+++ resources/views/components/m-agent-layout.blade.php
@@ -1,13 +1,20 @@
 @foreach ($agent as $agent)
+    @php
+        $address = $agent->company_address ?? null;
+        $addressDetail = $agent->company_address_detail ?? null;
+    @endphp
     <!-- list : s -->
     <a href="{{ route('www.map.agent.detail', [$agent->id]) }}">
         <div class="agent_sm_list">
             <div class="agent_sm_info">
-                <p class="agent_txt_item_1">주식회사더블유파트너즈부동산중개</p>
-                <p class="agent_txt_item_2">서울특별시 중구 세종대로 136 3층 S3002호</p>
+                <p class="agent_txt_item_1">{{ $agent->company_name }}</p>
+                <p class="agent_txt_item_2">{{ $address ? $address . ' ' . $addressDetail : '-' }} </p>
             </div>
+            {{ $agent }}
             <div class="frame_img_sm">
-                <div class="img_box"><img src="{{ asset('assets/media/default_img.png') }}"></div>
+                {{-- <div class="img_box"><img src="{{ Storage::url('image/' . $agent->images[0]->path) }}"
+                        onerror="this.src='{{ asset('assets/media/default_img.png') }}';"></div> --}}
+                {{-- <div class="img_box"><img src="{{ asset('assets/media/default_img.png') }}"></div> --}}
             </div>
         </div>
     </a>
