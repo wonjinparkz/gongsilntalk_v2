f3c11af8805ad0b98d3d5826f58bf7a7cdf2e78c
ggangeunbee <ggang@rocateer.com>
2024-06-24 15:51:48 +0900

    [config] session.php 수정

--- config/session.php
+++ config/session.php
@@ -33,7 +33,8 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120), // 세션 유지 시간 분단위
 
-    'expire_on_close' => true, // 세션 완료 시 동작
+    // 'expire_on_close' => true, // 세션 완료 시 동작
+    'expire_on_close' => false, // 세션 완료 시 동작
 
     // 'expired-session-redirect' => url(env('APP_URL')), //세션 만료시
 
