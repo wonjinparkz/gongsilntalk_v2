f530c59bef3af1e754ce67810ab4c5054cb5756c
준스 <junyeong@rocateer.com>
2024-06-26 11:31:29 +0900

    [사용자] 커뮤니티 상세 url 확인

--- resources/views/www/community/community_detail.blade.php
+++ resources/views/www/community/community_detail.blade.php
@@ -285,6 +285,7 @@
     <a id="create-kakaotalk-sharing-btn" href="javascript:;">
         <img src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
             alt="카카오톡 공유 보내기 버튼" />
+        {{ url()->full() }}
     </a>
 
     @php
