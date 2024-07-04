df75ebc8479914b4ad1fe05ba5e9f40d1f8b92fe
준스 <junyeong@rocateer.com>
2024-07-04 17:48:34 +0900

    [사용자] 모바일 마이페이지 상단 알림 모양 링크 수정

--- resources/views/www/mypage/productMagagement_list.blade.php
+++ resources/views/www/mypage/productMagagement_list.blade.php
@@ -4,7 +4,7 @@
     <div class="m_header">
         <div class="left_area">
             <a href="javascript:history.go(-1)">
-                <img src="http://localhost/assets/media/header_btn_back.png"></a>
+                <img src="{{ asset('assets/media/header_btn_back.png') }}"></a>
         </div>
         <div class="m_title">마이페이지</div>
         <div class="right_area">
