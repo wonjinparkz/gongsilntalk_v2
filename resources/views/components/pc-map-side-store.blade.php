342fe313d626e3f86b4e0b5089c0c2e2b1869d22
준스 <junyeong@rocateer.com>
2024-07-11 18:29:13 +0900

    [사용자] 실거래가지도 사이드 맵

--- resources/views/components/pc-map-side-store.blade.php
+++ resources/views/components/pc-map-side-store.blade.php
@@ -80,153 +80,156 @@
         }
     }
 @endphp
-<div class="side_header">
-    <div class="left_area"><a href="javascript:history.go(-1)"><img
-                src="{{ asset('assets/media/header_btn_back.png') }}"></a></div>
-    <div class="m_title">{{ $result->kstoreName }}</div>
-    <div class="right_area"><a href="#" class="btn_share"><img
-                src="{{ asset('assets/media/header_btn_share_deep.png') }}"></a></div>
-    <!-- 공유하기 : s -->
-    <div class="layer layer_share_wrap layer_share_top">
-        <div class="layer_title">
-            <h5>공유하기</h5>
-            <img src="{{ asset('assets/media/btn_md_close.png') }}" class="md_btn_close btn_share">
-        </div>
-        <div class="layer_share_con">
-            <a href="#">
-                <img src="{{ asset('assets/media/share_ic_01.png') }}">
-                <p class="mt8">카카오톡</p>
-            </a>
-            <a href="#">
-                <img src="{{ asset('assets/media/share_ic_02.png') }}">
-                <p class="mt8">링크복사</p>
-            </a>
-        </div>
-    </div>
-    <!-- 공유하기 : e -->
-</div>
-
-<div class="scroll_wrap_1">
-    <hr class="space">
-    <div class="estate_link">
-        <a href="{{ route('www.product.create.view') }}" class="flex_between">
-            <span><img src="{{ asset('assets/media/ic_org_estate.png') }}" class="ic_left_20"> 부동산 내놓기</span>
-            <span><img src="{{ asset('assets/media/ic_list_arrow.png') }}" class="ic_10"></span>
-        </a>
-    </div>
-    <hr class="space">
-
-    <div class="side_info_wrap">
-        <div>
-            <div id="minimap" style="width:100%; height:330px;" class="size_100p"></div>
-        </div>
-        <p class="txt_address">{{ $result->kstoreAddr }}</p>
-        <p class="txt_sub_1">{{ $result->subwayStation }} {{ $result->subwayLine }}
-            <span>{{ $result->kstoredWtimesub }}</span>
-        </p>
-        <ul class="info_detail">
-            <li>
-                <p>{{ $dongCount }}동</p><label>총 동수</label>
-            </li>
-            <li>
-                <p>{{ $mainUse }}</p><label>주용도</label>
-            </li>
-            <li>
-                <p>
-                    {{ $Minfloor > 0 ? 'B' . $Minfloor : ($floor > 0 ? $floorMin : '-') }}층/{{ $floor > 0 ? $floor : '-' }}층
-                </p>
-                <label>최저/최고</label>
-            </li>
-            <li>
-                <p>{{ $useAprDay }}년</p><label>준공년도</label>
-            </li>
-        </ul>
-    </div>
-
-    <!-- tab : s -->
-    <div class="tab_type_2 type_side">
-        <div style="width: 100%;">
-            <div class="swiper detail_tab">
-                <div class="swiper-wrapper menu toggle_menu">
-                    <div class="swiper-slide active"><a href="javascript:void(0);">가격·거래내역</a></div>
-                    <div class="swiper-slide"><a href="javascript:void(0);">건물·토지정보</a></div>
-                    <div class="swiper-slide"><a href="javascript:void(0);">위치 및 주변정보</a></div>
-                    <div class="swiper-slide"><a href="javascript:void(0);">매물정보</a></div>
-                </div>
+<!-- 전월세 있는 경우 transaction_type 추가 -->
+<div class="map_side_body">
+    <div class="side_header">
+        <div class="left_area"><a href="javascript:history.go(-1)"><img
+                    src="{{ asset('assets/media/header_btn_back.png') }}"></a></div>
+        <div class="m_title">{{ $result->kstoreName }}</div>
+        <div class="right_area"><a href="#" class="btn_share"><img
+                    src="{{ asset('assets/media/header_btn_share_deep.png') }}"></a></div>
+        <!-- 공유하기 : s -->
+        <div class="layer layer_share_wrap layer_share_top">
+            <div class="layer_title">
+                <h5>공유하기</h5>
+                <img src="{{ asset('assets/media/btn_md_close.png') }}" class="md_btn_close btn_share">
             </div>
-            <div class="swiper-button-next detail-tab-next"></div>
-            <!-- <div class="swiper-button-prev detail-tab-prev"></div> -->
-        </div>
-    </div>
-    <!-- tab : e -->
-
-    <div class="side_tab_wrap">
-        <div class="sction_item active">
-            <!-- 거래내역 : s -->
-            <div class="side_section">
-                <h4>거래내역</h4>
-                <!-- 데이터 없을 경우 -->
-                <div class="sm_type mt10">
-                    <button class="btn_point btn_md_full" onclick="location.href='//rtdown.molit.go.kr/'">실거래가
-                        보러가기</button>
-                </div>
+            <div class="layer_share_con">
+                <a href="#">
+                    <img src="{{ asset('assets/media/share_ic_01.png') }}">
+                    <p class="mt8">카카오톡</p>
+                </a>
+                <a href="#">
+                    <img src="{{ asset('assets/media/share_ic_02.png') }}">
+                    <p class="mt8">링크복사</p>
+                </a>
             </div>
-            <!-- 거래내역 : e -->
         </div>
-        <div class="sction_item">
-            <!-- 건물·토지정보 : s -->
-            <x-pc-buildingledger :BrTitleInfo="$BrTitleInfo" :BrRecapTitleInfo="$BrRecapTitleInfo" :BrFlrOulnInfo="$BrFlrOulnInfo" :BrExposInfo="$BrExposInfo"
-                :BrExposPubuseAreaInfo="$BrExposPubuseAreaInfo" :characteristics_json="$result->characteristics_json" :useWFS_json="$result->useWFS_json" />
-            <!-- 건물·토지정보 : e -->
+        <!-- 공유하기 : e -->
+    </div>
+
+    <div class="scroll_wrap_1">
+        <hr class="space">
+        <div class="estate_link">
+            <a href="{{ route('www.product.create.view') }}" class="flex_between">
+                <span><img src="{{ asset('assets/media/ic_org_estate.png') }}" class="ic_left_20"> 부동산 내놓기</span>
+                <span><img src="{{ asset('assets/media/ic_list_arrow.png') }}" class="ic_10"></span>
+            </a>
         </div>
-        <div class="sction_item">
-            <!-- 위치정보 : s -->
-            <div class="side_section">
-                <h4>위치 및 주변정보</h4>
-                <div class="container_map_wrap mt18">
-                    <x-pc-around-map :address_lat="$result->address_lat" :address_lng="$result->address_lng" />
+        <hr class="space">
+
+        <div class="side_info_wrap">
+            <div>
+                <div id="minimap" style="width:100%; height:330px;" class="size_100p"></div>
+            </div>
+            <p class="txt_address">{{ $result->kstoreAddr }}</p>
+            <p class="txt_sub_1">{{ $result->subwayStation }} {{ $result->subwayLine }}
+                <span>{{ $result->kstoredWtimesub }}</span>
+            </p>
+            <ul class="info_detail">
+                <li>
+                    <p>{{ $dongCount }}동</p><label>총 동수</label>
+                </li>
+                <li>
+                    <p>{{ $mainUse }}</p><label>주용도</label>
+                </li>
+                <li>
+                    <p>
+                        {{ $Minfloor > 0 ? 'B' . $Minfloor : ($floor > 0 ? $floorMin : '-') }}층/{{ $floor > 0 ? $floor : '-' }}층
+                    </p>
+                    <label>최저/최고</label>
+                </li>
+                <li>
+                    <p>{{ $useAprDay }}년</p><label>준공년도</label>
+                </li>
+            </ul>
+        </div>
+
+        <!-- tab : s -->
+        <div class="tab_type_2 type_side">
+            <div style="width: 100%;">
+                <div class="swiper detail_tab">
+                    <div class="swiper-wrapper menu toggle_menu">
+                        <div class="swiper-slide active"><a href="javascript:void(0);">가격·거래내역</a></div>
+                        <div class="swiper-slide"><a href="javascript:void(0);">건물·토지정보</a></div>
+                        <div class="swiper-slide"><a href="javascript:void(0);">위치 및 주변정보</a></div>
+                        <div class="swiper-slide"><a href="javascript:void(0);">매물정보</a></div>
+                    </div>
                 </div>
-                <div class="map_detail_wrp">
-                    <ul class="tab_sm_menu tab_type_4">
-                        <li class="active"><a href="javascript:(0)">대중교통</a></li>
-                        <li><a href="javascript:(0)">편의시설</a></li>
-                        <li><a href="javascript:(0)">교육시설</a></li>
-                    </ul>
-                    <div class="tab_sm_wrap">
-                        <div class="traffic_wrap">
-                            <div class="traffic_tit"><img src="{{ asset('assets/media/ic_subway.png') }}">지하철
-                            </div>
-                            <p class="traffic_row">{{ $result->subwayStation }} {{ $result->subwayLine }}
-                                <span>{{ $result->kaptdWtimesub }}</span>
-                            </p>
+                <div class="swiper-button-next detail-tab-next"></div>
+                <!-- <div class="swiper-button-prev detail-tab-prev"></div> -->
+            </div>
+        </div>
+        <!-- tab : e -->
 
-                            <div class="traffic_tit mt28"><img src="{{ asset('assets/media/ic_bus.png') }}">버스
-                            </div>
-                            <p class="traffic_row">정류장 <span>{{ $result->kaptdWtimebus }}</span></p>
+        <div class="side_tab_wrap">
+            <div class="sction_item active">
+                <!-- 거래내역 : s -->
+                <div class="side_section">
+                    <h4>거래내역</h4>
+                    <!-- 데이터 없을 경우 -->
+                    <div class="sm_type mt10">
+                        <button class="btn_point btn_md_full" onclick="location.href='//rtdown.molit.go.kr/'">실거래가
+                            보러가기</button>
+                    </div>
+                </div>
+                <!-- 거래내역 : e -->
+            </div>
+            <div class="sction_item">
+                <!-- 건물·토지정보 : s -->
+                <x-pc-buildingledger :BrTitleInfo="$BrTitleInfo" :BrRecapTitleInfo="$BrRecapTitleInfo" :BrFlrOulnInfo="$BrFlrOulnInfo" :BrExposInfo="$BrExposInfo"
+                    :BrExposPubuseAreaInfo="$BrExposPubuseAreaInfo" :characteristics_json="$result->characteristics_json" :useWFS_json="$result->useWFS_json" />
+                <!-- 건물·토지정보 : e -->
+            </div>
+            <div class="sction_item">
+                <!-- 위치정보 : s -->
+                <div class="side_section">
+                    <h4>위치 및 주변정보</h4>
+                    <div class="container_map_wrap mt18">
+                        <x-pc-around-map :address_lat="$result->address_lat" :address_lng="$result->address_lng" />
+                    </div>
+                    <div class="map_detail_wrp">
+                        <ul class="tab_sm_menu tab_type_4">
+                            <li class="active"><a href="javascript:(0)">대중교통</a></li>
+                            <li><a href="javascript:(0)">편의시설</a></li>
+                            <li><a href="javascript:(0)">교육시설</a></li>
+                        </ul>
+                        <div class="tab_sm_wrap">
+                            <div class="traffic_wrap">
+                                <div class="traffic_tit"><img src="{{ asset('assets/media/ic_subway.png') }}">지하철
+                                </div>
+                                <p class="traffic_row">{{ $result->subwayStation }} {{ $result->subwayLine }}
+                                    <span>{{ $result->kaptdWtimesub }}</span>
+                                </p>
 
-                        </div>
-                        <div>
-                            <div class="facility_wrap">
-                                {!! nl2br($result->convenientFacility) !!}
+                                <div class="traffic_tit mt28"><img src="{{ asset('assets/media/ic_bus.png') }}">버스
+                                </div>
+                                <p class="traffic_row">정류장 <span>{{ $result->kaptdWtimebus }}</span></p>
+
                             </div>
-                        </div>
-                        <div>
-                            <div class="edu_wrap">
-                                {!! nl2br($result->educationFacility) !!}
+                            <div>
+                                <div class="facility_wrap">
+                                    {!! nl2br($result->convenientFacility) !!}
+                                </div>
+                            </div>
+                            <div>
+                                <div class="edu_wrap">
+                                    {!! nl2br($result->educationFacility) !!}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <!-- 위치정보 : s -->
             </div>
-            <!-- 위치정보 : s -->
+            <div class="sction_item">
+                <!-- 매물정보 : s -->
+                <x-pc-map-product-list :productList="$result->productList" />
+                <!-- 매물정보 : e -->
+            </div>
         </div>
-        <div class="sction_item">
-            <!-- 매물정보 : s -->
-            <x-pc-map-product-list :productList="$result->productList" />
-            <!-- 매물정보 : e -->
-        </div>
-    </div>
 
+    </div>
 </div>
 
 
