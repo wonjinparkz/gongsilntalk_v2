d953ec209db70361ca9cf256f6557b30303117e2
준스 <junyeong@rocateer.com>
2024-07-11 16:18:01 +0900

    [사용자] 내 자산 관리

--- resources/views/www/mypage/service_list.blade.php
+++ resources/views/www/mypage/service_list.blade.php
@@ -196,7 +196,7 @@
                                                 <td><span class="txt_point">{{ number_format($myPrice) }}원</span>
                                                 </td>
                                                 <td><span
-                                                        class="txt_point">{{ round(($myPrice / $realPrice) * 100, 2) }}%</span>
+                                                        class="txt_point">{{ round((($myPrice * 12) / $realPrice) * 100, 2) }}%</span>
                                                 </td>
                                                 <td><img src="{{ asset('assets/media/ic_list_arrow.png') }}"
                                                         class="w_8p">
@@ -262,7 +262,7 @@
                                                     class="txt_point">{{ number_format($myPrice) }}원</span>
                                             </div>
                                             <div class="list_detail_item">수익률 <span
-                                                    class="txt_point">{{ round(($myPrice / $realPrice) * 100, 2) }}%</span>
+                                                    class="txt_point">{{ round((($myPrice * 12) / $realPrice) * 100, 2) }}%</span>
                                             </div>
                                             <button class="btn_graylight_ghost btn_sm_full mt10"
                                                 onclick="location.href='{{ route('www.mypage.service.detail.view', [$asset->id]) }}'">자세히보기</button>
