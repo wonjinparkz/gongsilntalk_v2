24722008244e1d1f7573bc7511325090d8f137af
준스 <junyeong@rocateer.com>
2024-07-12 15:55:32 +0900

    [사용자] 매물 제안서 상세 진입 불가 수정 작업 중

--- resources/views/www/proposal/my-proposal-offer-list.blade.php
+++ resources/views/www/proposal/my-proposal-offer-list.blade.php
@@ -330,11 +330,11 @@
                                                                 $product->product->priceInfo->payment_type == 2 ||
                                                                 $product->product->priceInfo->payment_type == 4
                                                             ) {
-                                                                $monthPrice =
-                                                                    ' / ' .
-                                                                    priceChange(
-                                                                        $product->product->priceInfo->month_price,
-                                                                    );
+                                                                // $monthPrice =
+                                                                //     ' / ' .
+                                                                //     priceChange(
+                                                                //         $product->product->priceInfo->month_price,
+                                                                //     );
                                                                 $priceArea =
                                                                     $product->product->priceInfo->month_price /
                                                                     $product->product->exclusive_area;
