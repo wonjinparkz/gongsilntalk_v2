26423ba04600e511161f4807922906a364925ebf
준스 <junyeong@rocateer.com>
2024-07-12 15:57:20 +0900

    [사용자] 매물 제안서 상세 진입 불가 수정 작업 중

--- resources/views/www/proposal/my-proposal-offer-list.blade.php
+++ resources/views/www/proposal/my-proposal-offer-list.blade.php
@@ -217,23 +217,23 @@
                                                     @php
                                                         $monthPrice = '';
                                                         $priceArea = 0.0;
-                                                        if (
-                                                            $product->product->priceInfo->payment_type == 1 ||
-                                                            $product->product->priceInfo->payment_type == 2 ||
-                                                            $product->product->priceInfo->payment_type == 4
-                                                        ) {
-                                                            // $monthPrice =
-                                                            //     ' / ' .
-                                                            //     priceChange($product->product->priceInfo->month_price);
-                                                            $priceArea =
-                                                                $product->product->priceInfo->month_price /
-                                                                $product->product->exclusive_area;
-                                                        } else {
-                                                            $monthPrice = '';
-                                                            $priceArea =
-                                                                $product->product->priceInfo->price /
-                                                                $product->product->exclusive_area;
-                                                        }
+                                                        // if (
+                                                        //     $product->product->priceInfo->payment_type == 1 ||
+                                                        //     $product->product->priceInfo->payment_type == 2 ||
+                                                        //     $product->product->priceInfo->payment_type == 4
+                                                        // ) {
+                                                        //     $monthPrice =
+                                                        //         ' / ' .
+                                                        //         priceChange($product->product->priceInfo->month_price);
+                                                        //     $priceArea =
+                                                        //         $product->product->priceInfo->month_price /
+                                                        //         $product->product->exclusive_area;
+                                                        // } else {
+                                                        //     $monthPrice = '';
+                                                        //     $priceArea =
+                                                        //         $product->product->priceInfo->price /
+                                                        //         $product->product->exclusive_area;
+                                                        // }
                                                     @endphp
                                                     <span>{{ Lang::get('commons.payment_type.' . $product->product->priceInfo->payment_type) }}
                                                         {{ priceChange($product->product->priceInfo->price) }}
@@ -325,25 +325,25 @@
                                                         @php
                                                             $monthPrice = '';
                                                             $priceArea = 0.0;
-                                                            if (
-                                                                $product->product->priceInfo->payment_type == 1 ||
-                                                                $product->product->priceInfo->payment_type == 2 ||
-                                                                $product->product->priceInfo->payment_type == 4
-                                                            ) {
-                                                                // $monthPrice =
-                                                                //     ' / ' .
-                                                                //     priceChange(
-                                                                //         $product->product->priceInfo->month_price,
-                                                                //     );
-                                                                $priceArea =
-                                                                    $product->product->priceInfo->month_price /
-                                                                    $product->product->exclusive_area;
-                                                            } else {
-                                                                $monthPrice = '';
-                                                                $priceArea =
-                                                                    $product->product->priceInfo->price /
-                                                                    $product->product->exclusive_area;
-                                                            }
+                                                            // if (
+                                                            //     $product->product->priceInfo->payment_type == 1 ||
+                                                            //     $product->product->priceInfo->payment_type == 2 ||
+                                                            //     $product->product->priceInfo->payment_type == 4
+                                                            // ) {
+                                                            //     $monthPrice =
+                                                            //         ' / ' .
+                                                            //         priceChange(
+                                                            //             $product->product->priceInfo->month_price,
+                                                            //         );
+                                                            //     $priceArea =
+                                                            //         $product->product->priceInfo->month_price /
+                                                            //         $product->product->exclusive_area;
+                                                            // } else {
+                                                            //     $monthPrice = '';
+                                                            //     $priceArea =
+                                                            //         $product->product->priceInfo->price /
+                                                            //         $product->product->exclusive_area;
+                                                            // }
 
                                                         @endphp
                                                         {{ Lang::get('commons.payment_type.' . $product->product->priceInfo->payment_type) }}
