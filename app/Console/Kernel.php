5045827886b8e78087107957fd07e9cb92d9f25f
준스 <junyeong@rocateer.com>
2024-07-22 16:25:52 +0900

    [시스템] 스케쥴러 데이터베이스 연결 오류

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Http\Controllers\data\DataController;
+use App\Models\DataApt;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
@@ -16,13 +17,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('app:get-apt-info')->everyMinute();
-        // $schedule->command('app:get-building-ledger')->everyMinute();
         // $schedule->command('app:send-siteProduct-alarm')->dailyAt('00:00');
+        // $schedule->command('app:get-building-ledger')->everyMinute();
 
 
         $schedule->call(function () {
             Log::info('Scheduler is running.');
-            DB::table('data_apt')->where('is_base_info', 0)->limit(1)->get();
+            $DetailInfo = DataApt::where('is_detail_info', 0)->first();
+            Log::info($DetailInfo);
         })->everyMinute();
     }
 
