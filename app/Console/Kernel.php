4339d0f8b6394c99a6368d4e1b95e37dff97f3de
준스 <junyeong@rocateer.com>
2024-07-22 16:49:50 +0900

    [시스템] 스케쥴러 데이터베이스 연결 오류

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,18 +17,18 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('app:get-apt-info')->everyMinute();
+        $schedule->command('app:get-apt-info')->everyMinute();
         // $schedule->command('app:send-siteProduct-alarm')->dailyAt('00:00');
         // $schedule->command('app:get-building-ledger')->everyMinute();
 
 
-        $schedule->call(function () {
-            Log::info('Scheduler is running.');
-            User::whereDate('id', 6)
-                ->update([
-                    "type" => 1
-                ]);
-        })->everyMinute();
+        // $schedule->call(function () {
+        //     Log::info('Scheduler is running.');
+        //     User::whereDate('id', 6)
+        //         ->update([
+        //             "type" => 1
+        //         ]);
+        // })->everyMinute();
     }
 
     /**
