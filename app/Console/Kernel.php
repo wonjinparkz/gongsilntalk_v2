1b335cc275d9ca693de6c317aa7b0316c8172f09
준스 <junyeong@rocateer.com>
2024-07-23 09:34:45 +0900

    [시스템] 스케쥴러 실행

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,12 +3,8 @@
 namespace App\Console;
 
 use App\Http\Controllers\data\DataController;
-use App\Models\DataApt;
-use App\Models\User;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -18,17 +14,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:get-apt-info')->everyMinute();
-        // $schedule->command('app:send-siteProduct-alarm')->dailyAt('00:00');
-        // $schedule->command('app:get-building-ledger')->everyMinute();
+        $schedule->command('app:get-building-ledger')->everyMinute();
+        $schedule->command('app:send-siteProduct-alarm')->dailyAt('00:00');
 
-
-        // $schedule->call(function () {
-        //     Log::info('Scheduler is running.');
-        //     User::whereDate('id', 6)
-        //         ->update([
-        //             "type" => 1
-        //         ]);
-        // })->everyMinute();
+        $schedule->call(function () {
+        })->everyMinute();
     }
 
     /**
