f204c5efcfa499b1599c0f3b871f8460e45e6891
준스 <junyeong@rocateer.com>
2024-07-23 13:14:36 +0900

    [시스템] 아파트 표제부 세팅 여부 쿼리 수정

--- app/Http/Controllers/data/DataController.php
+++ app/Http/Controllers/data/DataController.php
@@ -790,6 +790,7 @@ class DataController extends Controller
     {
 
         $apt = DataApt::whereRaw('CHAR_LENGTH(pnu) = 19')
+            ->where('is_building_ledger', 0)
             ->where(function ($query) {
                 $query->whereDoesntHave('BrTitleInfo')
                     ->orWhereDoesntHave('BrRecapTitleInfo')
