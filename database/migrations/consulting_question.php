2b855e2066788ef303e4d2d39879b0f740cd419f
준스 <junyeong@rocateer.com>
2024-07-29 18:27:58 +0900

    [db]sail artisan migrate:refresh --path=database/migrations/consulting_question.php

--- database/migrations/consulting_question.php
+++ database/migrations/consulting_question.php
@@ -12,16 +12,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cosulting_question', function (Blueprint $table) {
+        Schema::create('consulting_question', function (Blueprint $table) {
             $table->id()->comment('컨설팅 문의 아이디');
             $table->string('name')->comment('기업 명');
             $table->string('phone')->comment('기업 명');
             $table->string('email')->comment('기업 명');
             $table->longText('content')->comment('기업 명');
+            $table->integer('state')->comment('상태');
+            $table->integer('is_delete')->comment('문의 삭제 상태 - 0 : 게시중, 1 : 삭제 ');
             $table->timestamps();
         });
 
-        DB::statement("ALTER TABLE cosulting_question COMMENT='컨설팅 문의'");
+        DB::statement("ALTER TABLE consulting_question COMMENT='컨설팅 문의'");
     }
 
     /**
@@ -29,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cosulting_question');
+        Schema::dropIfExists('consulting_question');
     }
 };
