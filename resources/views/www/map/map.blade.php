37e7f27dd6c0db56105f3b8fcf7b3bbeee040b61
준스 <junyeong@rocateer.com>
2024-07-31 11:51:10 +0900

    [사용자] pc 매물지도 중개사 좌표 수정

--- resources/views/www/map/map.blade.php
+++ resources/views/www/map/map.blade.php
@@ -536,8 +536,8 @@
             } = item;
             createAgentMarker({
                 id: item.id,
-                lat: company_address_lat + 0.000010,
-                lng: company_address_lng + 0.000010,
+                lat: company_address_lat + 0.10,
+                lng: company_address_lng + 0.10,
                 type: type,
                 image: image,
                 anchorX: anchorX,
