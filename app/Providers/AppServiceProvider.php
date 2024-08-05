fb907bb3508ce12d05012ec440de0ebdca606428
준스 <junyeong@rocateer.com>
2024-08-05 09:39:23 +0900

    [사용자] ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', 'on');
+        }
+
+
         // DB 로그
         DB::listen(function ($query) {
 
