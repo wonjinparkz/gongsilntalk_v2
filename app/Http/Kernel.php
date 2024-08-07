b70a1bf902ab25c6ef104ef7dc5a2ebf7685f795
준스 <junyeong@rocateer.com>
2024-08-07 14:59:15 +0900

    [설정] 자동로그인 미들웨어 추가

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\AutoLogin::class, // 자동 로그인 미들웨어 추가
     ];
 
     /**
