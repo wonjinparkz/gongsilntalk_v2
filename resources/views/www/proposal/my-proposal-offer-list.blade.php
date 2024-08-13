30f34e740552741fd9a1ecbd6f7a86674e028de2
준스 <junyeong@rocateer.com>
2024-08-13 16:43:42 +0900

    [사용자] 내 재산 상세 불필요한 코드 삭제

--- resources/views/www/proposal/my-proposal-offer-list.blade.php
+++ resources/views/www/proposal/my-proposal-offer-list.blade.php
@@ -4,9 +4,6 @@
     <script type="text/javascript"
         src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId={{ env('VITE_NAVER_MAP_CLIENT_ID') }}&submodules=panorama">
     </script>
-    @foreach ($proposal->products as $product)
-        {{ Log::info($product->product->priceInfo ?? '엉?') }}
-    @endforeach
 
     <!----------------------------- m::header bar : s ----------------------------->
     <div class="m_header">
