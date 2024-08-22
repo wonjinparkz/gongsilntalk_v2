adc24320c84d5a2dffe2e3b11381cb5e512e37e6
준스 <junyeong@rocateer.com>
2024-08-22 17:17:26 +0900

    [사용자] 공유페이지 마크 추가

--- resources/views/www/sharePage/calculator_revenue_share_page.blade.php
+++ resources/views/www/sharePage/calculator_revenue_share_page.blade.php
@@ -59,129 +59,132 @@
         </div>
 
         <div class="share_section">
-            <div class="revenue_item">
-                @php
+            <div class="calculator_container">
+                <div class="revenue_item">
+                    @php
 
-                    $sale_price = $calculator->sale_price;
-                    $acquisition_tax = $calculator->acquisition_tax;
-                    $tax_price = $calculator->tax_price ?? 0;
-                    $commission = $calculator->commission ?? 0;
-                    $etc_price = $calculator->etc_price ?? 0;
-                    $price = $calculator->price ?? 0;
-                    $month_price = $calculator->month_price ?? 0;
-                    $loan_ratio = $calculator->loan_ratio ?? 0;
-                    $loan_interest = $calculator->loan_interest ?? 0;
+                        $sale_price = $calculator->sale_price;
+                        $acquisition_tax = $calculator->acquisition_tax;
+                        $tax_price = $calculator->tax_price ?? 0;
+                        $commission = $calculator->commission ?? 0;
+                        $etc_price = $calculator->etc_price ?? 0;
+                        $price = $calculator->price ?? 0;
+                        $month_price = $calculator->month_price ?? 0;
+                        $loan_ratio = $calculator->loan_ratio ?? 0;
+                        $loan_interest = $calculator->loan_interest ?? 0;
 
-                    // 취득세
-                    $acquisition_tax_price = $sale_price * ($acquisition_tax / 100);
+                        // 취득세
+                        $acquisition_tax_price = $sale_price * ($acquisition_tax / 100);
 
-                    // 기타비용
-                    $etc_price_sum = $tax_price + $commission + $etc_price;
+                        // 기타비용
+                        $etc_price_sum = $tax_price + $commission + $etc_price;
 
-                    // 대출금
-                    $loan_price = $sale_price * ($loan_ratio / 100);
+                        // 대출금
+                        $loan_price = $sale_price * ($loan_ratio / 100);
 
-                    // 월 이자 상환액
-                    $month_interest_price = ($loan_price * ($loan_interest / 100)) / 12;
+                        // 월 이자 상환액
+                        $month_interest_price = ($loan_price * ($loan_interest / 100)) / 12;
 
-                    // 실투자금
-                    $investment_price = $sale_price + $acquisition_tax_price + $etc_price_sum - $loan_price - $price;
+                        // 실투자금
+                        $investment_price =
+                            $sale_price + $acquisition_tax_price + $etc_price_sum - $loan_price - $price;
 
-                    // 월 순수익
-                    $month_revenue_price = $month_price - $month_interest_price;
+                        // 월 순수익
+                        $month_revenue_price = $month_price - $month_interest_price;
 
-                    // 연 순수익
-                    $revenue_price = $month_revenue_price * 12;
+                        // 연 순수익
+                        $revenue_price = $month_revenue_price * 12;
 
-                    // // 수익률
-                    // $revenue_rate = ($revenue_price / $investment_price) * 100;
+                        // // 수익률
+                        // $revenue_rate = ($revenue_price / $investment_price) * 100;
 
-                    // // 실투자금 회수기간
-                    // $payback_period = $investment_price / $revenue_price;
-                    // 수익률
-                    $revenue_rate = $investment_price != 0 ? ($revenue_price / $investment_price) * 100 : 0;
+                        // // 실투자금 회수기간
+                        // $payback_period = $investment_price / $revenue_price;
+                        // 수익률
+                        $revenue_rate = $investment_price != 0 ? ($revenue_price / $investment_price) * 100 : 0;
 
-                    // 실투자금 회수기간
-                    $payback_period = $revenue_price != 0 ? $investment_price / $revenue_price : 0;
-                @endphp
-                <div class="table_container columns_2 mt18">
-                    <div class="td">매매/분양가</div>
-                    <div class="td">
-                        {{ number_format($sale_price) }}원
-                    </div>
-                    <div class="td">
-                        취득세
-                    </div>
-                    <div class="td">
-                        {{ number_format($acquisition_tax_price) }}원
-                        <span class="txt_point">({{ $acquisition_tax }}%)</span>
-                    </div>
-                    <div class="td">
-                        기타비용
-                    </div>
-                    <div class="td">
-                        {{ number_format($etc_price_sum) }}원
-                    </div>
-                    <div class="td">
-                        대출금
-                    </div>
-                    <div class="td">
-                        {{ number_format($loan_price) }}원
-                        <span class="txt_point">({{ $loan_ratio }}%)</span>
-                    </div>
-                    <div class="td">
-                        대출금리
-                    </div>
-                    <div class="td">
-                        {{ number_format($loan_interest, 2) }}%
-                    </div>
-                    <div class="td">
-                        월 이자 상환액
-                    </div>
-                    <div class="td">
-                        {{ number_format($month_interest_price) }}원
-                    </div>
-                    <div class="td">
-                        보증금
-                    </div>
-                    <div class="td">
-                        {{ number_format($price) }}원
-                    </div>
-                    <div class="td">
-                        월임대료
-                    </div>
-                    <div class="td">
-                        {{ number_format($month_price) }}원
-                    </div>
-                    <div class="td">
-                        실투자금
-                    </div>
-                    <div class="td">
-                        <span class="txt_point">{{ number_format($investment_price) }}원</span>
-                    </div>
-                    <div class="td">
-                        월순수익
-                    </div>
-                    <div class="td">
-                        <span class="txt_point">{{ number_format($month_revenue_price) }}원</span>
-                    </div>
-                    <div class="td">
-                        연순수익
-                    </div>
-                    <div class="td">
-                        {{ number_format($revenue_price) }}원
-                    </div>
-                    <div class="td">
-                        수익률
-                    </div>
-                    <div class="td">
-                        <span class="txt_point">{{ number_format($revenue_rate, 2) }}%</span>
-                    </div>
-                    <div class="td">
-                        실투자금 회수기간
-                    </div>
-                    <div class="td">
-                        {{ $payback_period > 0 ? number_format($payback_period, 1) : '-' }}년
+                        // 실투자금 회수기간
+                        $payback_period = $revenue_price != 0 ? $investment_price / $revenue_price : 0;
+                    @endphp
+                    <div class="table_container columns_2 mt18">
+                        <div class="td">매매/분양가</div>
+                        <div class="td">
+                            {{ number_format($sale_price) }}원
+                        </div>
+                        <div class="td">
+                            취득세
+                        </div>
+                        <div class="td">
+                            {{ number_format($acquisition_tax_price) }}원
+                            <span class="txt_point">({{ $acquisition_tax }}%)</span>
+                        </div>
+                        <div class="td">
+                            기타비용
+                        </div>
+                        <div class="td">
+                            {{ number_format($etc_price_sum) }}원
+                        </div>
+                        <div class="td">
+                            대출금
+                        </div>
+                        <div class="td">
+                            {{ number_format($loan_price) }}원
+                            <span class="txt_point">({{ $loan_ratio }}%)</span>
+                        </div>
+                        <div class="td">
+                            대출금리
+                        </div>
+                        <div class="td">
+                            {{ number_format($loan_interest, 2) }}%
+                        </div>
+                        <div class="td">
+                            월 이자 상환액
+                        </div>
+                        <div class="td">
+                            {{ number_format($month_interest_price) }}원
+                        </div>
+                        <div class="td">
+                            보증금
+                        </div>
+                        <div class="td">
+                            {{ number_format($price) }}원
+                        </div>
+                        <div class="td">
+                            월임대료
+                        </div>
+                        <div class="td">
+                            {{ number_format($month_price) }}원
+                        </div>
+                        <div class="td">
+                            실투자금
+                        </div>
+                        <div class="td">
+                            <span class="txt_point">{{ number_format($investment_price) }}원</span>
+                        </div>
+                        <div class="td">
+                            월순수익
+                        </div>
+                        <div class="td">
+                            <span class="txt_point">{{ number_format($month_revenue_price) }}원</span>
+                        </div>
+                        <div class="td">
+                            연순수익
+                        </div>
+                        <div class="td">
+                            {{ number_format($revenue_price) }}원
+                        </div>
+                        <div class="td">
+                            수익률
+                        </div>
+                        <div class="td">
+                            <span class="txt_point">{{ number_format($revenue_rate, 2) }}%</span>
+                        </div>
+                        <div class="td">
+                            실투자금 회수기간
+                        </div>
+                        <div class="td">
+                            {{ $payback_period > 0 ? number_format($payback_period, 1) : '-' }}년
+                        </div>
                     </div>
                 </div>
             </div>
