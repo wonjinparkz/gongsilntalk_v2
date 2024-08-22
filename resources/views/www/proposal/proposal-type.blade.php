f56b5757c6c6e424f377d222e60d98d412f0c4a2
준스 <junyeong@rocateer.com>
2024-08-22 12:04:37 +0900

    [사용자] 기업이전 제안서 공유 페이지

--- resources/views/www/proposal/proposal-type.blade.php
+++ resources/views/www/proposal/proposal-type.blade.php
@@ -22,6 +22,32 @@
                     <div class="inner_wrap m_inner_wrap">
                         <h1 class="t_center only_pc">제안서 미리보기</h1>
                     </div>
+                    <!-- 공유하기 : s -->
+                    <div class="relative">
+                        <button class="btn_graylight_ghost btn_sm btn_share" data-share="share_entre"><img
+                                src="{{ asset('assets/media/header_btn_share_deep.png') }}" class="normal"></button>
+
+                        <div class="layer layer_share_wrap layer_share_top share_entre">
+                            <div class="layer_title">
+                                <h5>공유하기</h5>
+                                <img src="{{ asset('assets/media/btn_md_close.png') }}" class="md_btn_close btn_share"
+                                    data-share="share_entre">
+                            </div>
+                            <div class="layer_share_con">
+                                <a class="kakaotalk-sharing-btn">
+                                    <img src="{{ asset('assets/media/share_ic_01.png') }}">
+                                    <p class="mt8">카카오톡</p>
+                                </a>
+                                <a
+                                    onclick="textCopy('{{ env('APP_URL') }}/share/corp/proposal/detail?id={{ $corpInfo->id }}&proposal_type=' + $('input[name=proposal_type]:checked').val());$('.md_btn_close').click();">
+                                    <img src="{{ asset('assets/media/share_ic_02.png') }}">
+                                    <p class="mt8">링크복사</p>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- 공유하기 : e -->
 
                     <div class="proposal_type_wrap">
                         <!-- tab : s -->
@@ -79,8 +105,6 @@
                         </div>
 
                     </div>
-
-
                 </div>
                 <!-- my_body : e -->
 
@@ -91,6 +115,42 @@
     </div>
 
     <script>
+        var firstTab = $('.proposal_type_tab input[name="proposal_type"]').first();
+        if (firstTab.length) {
+            firstTab.prop('checked', true); // 첫 번째 라디오 버튼 선택
+            showType(firstTab[0]); // 해당 탭의 내용을 보여주는 함수 호출
+        }
+
+        //공유하기 레이어
+        $(".btn_share").click(function() {
+            var shareLayerClass = $(this).data("share");
+            $("." + shareLayerClass).stop().slideToggle(0);
+            return;
+        });
+
+        var textCopy = (url) => {
+            window.navigator.clipboard.writeText(url).then(() => {
+                alert("링크가 복사 되었습니다.");
+            });
+        };
+
+        document.querySelectorAll('.kakaotalk-sharing-btn').forEach(function(button) {
+            Kakao.Share.createDefaultButton({
+                container: button,
+                objectType: "feed",
+                content: {
+                    title: '{{ $corpInfo->corp_name }} 기업 이전 제안서',
+                    description: '{{ $corpInfo->corp_name }} 기업 이전 제안서를 공유드립니다.',
+                    imageUrl: "",
+                    link: {
+                        mobileWebUrl: `{{ env('APP_URL') }}/share/corp/proposal/detail?id={{ $corpInfo->id }}&type=${$('input[name=proposal_type]:checked').val()}`,
+                        webUrl: `{{ env('APP_URL') }}/share/corp/proposal/detail?id={{ $corpInfo->id }}&type=${$('input[name=proposal_type]:checked').val()}`,
+                    },
+                }
+            });
+        });
+
+
         //탭 보기
         function showType(element) {
             var is_checked = element.checked;
