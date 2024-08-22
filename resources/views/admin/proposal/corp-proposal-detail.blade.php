56b0f69db416e24a37b46eb70fe15fc70473e77d
준스 <junyeong@rocateer.com>
2024-08-22 12:27:22 +0900

    [관리자] 기업 이전 제안서 상세 > 중개사 프로필

--- resources/views/admin/proposal/corp-proposal-detail.blade.php
+++ resources/views/admin/proposal/corp-proposal-detail.blade.php
@@ -26,8 +26,7 @@
                         <div class="symbol symbol-150px symbol-circle mb-5">
                             @if (count($result->users->images) > 0)
                                 @foreach ($result->users->images as $image)
-                                    {{ $image }}
-                                    {{-- <img src="{{ Storage::url('image/' . $images->path) }}" /> --}}
+                                    <img src="{{ Storage::url('image/' . $image->path) }}" />
                                 @endforeach
                             @else
                                 <img src="{{ asset('assets/media/default_user.png') }}" />
