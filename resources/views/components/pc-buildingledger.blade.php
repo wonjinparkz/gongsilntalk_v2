f546d6b1e732b3f678be2d52e8cecde11829a98a
준스 <junyeong@rocateer.com>
2024-08-27 16:42:10 +0900

    [사용자] 배열 또는 단일로 들어오는 데이터 단일로 바꾸기

--- resources/views/components/pc-buildingledger.blade.php
+++ resources/views/components/pc-buildingledger.blade.php
@@ -82,7 +82,7 @@
                                     {{ $info['regstrGbCdNm'] != '' ? '(' . $info['regstrGbCdNm'] . ')' : '' }}
                                 </td>
                                 <td class="txt_sm">{{ $info['dongNm'] ?? '-' }}</td>
-                                <td class="txt_sm">{{ Commons::formatValue($info['mainPurpsCdNm']) }}</td>
+                                <td class="txt_sm">{{ Commons::formatValue($info['mainPurpsCdNm'] ?? '') }}</td>
                                 <td class="txt_sm">
                                     {{ $info['archArea'] != '' ? number_format($info['archArea'], 2) . '㎡' : '-' }}
                                 </td>
@@ -123,7 +123,7 @@
                     <div class="td">주구조</div>
                     <div class="td">{{ $info['strctCdNm'] ?? '-' }}</div>
                     <div class="td">지붕구조</div>
-                    <div class="td">{{ Commons::formatValue($info['etcRoof']) }}</div>
+                    <div class="td">{{ Commons::formatValue($info['etcRoof'] ?? '') }}</div>
                     <div class="td">엘리베이터</div>
                     <div class="td">{{ $info['rideUseElvtCnt'] ?? '-' }}</div>
                     <div class="td">용적률</div>
@@ -166,7 +166,7 @@
                     <div class="td">주구조</div>
                     <div class="td">{{ $info['strctCdNm'] ?? '-' }}</div>
                     <div class="td">지붕구조</div>
-                    <div class="td">{{ Commons::formatValue($info['etcRoof']) }}</div>
+                    <div class="td">{{ Commons::formatValue($info['etcRoof'] ?? '') }}</div>
                     <div class="td">엘리베이터</div>
                     <div class="td">{{ $info['rideUseElvtCnt'] > 0 ? '총 ' . $info['rideUseElvtCnt'] . '대' : '-' }}
                     </div>
@@ -206,7 +206,8 @@
                                 @if ($name == $info['dongNm'])
                                     <tr>
                                         <td>{{ $info['flrNoNm'] }}</td>
-                                        <td>{{ is_array($info['mainPurpsCdNm']) ? implode(', ', $info['mainPurpsCdNm']) : ($info['mainPurpsCdNm'] ?? '-') }}</td>
+                                        <td>{{ is_array($info['mainPurpsCdNm']) ? implode(', ', $info['mainPurpsCdNm']) : $info['mainPurpsCdNm'] ?? '-' }}
+                                        </td>
                                         <td>{{ number_format($info['area'], 2) }}㎡</td>
                                     </tr>
                                 @endif
@@ -281,7 +282,7 @@
                                     <td>{{ $info['exposPubuseGbCdNm'] }}</td>
                                     <td>{{ isset($info['dongNm']) && $info['dongNm'] !== '' ? '' : $info['dongNm'] }}
                                     </td>
-                                    <td>{{ Commons::formatValue($info['mainPurpsCdNm']) }}</td>
+                                    <td>{{ Commons::formatValue($info['mainPurpsCdNm'] ?? '') }}</td>
                                     <td>{{ $info['mainAtchGbCdNm'] }}</td>
                                     <td>{{ number_format($info['area']) }}㎡</td>
                                 </tr>
