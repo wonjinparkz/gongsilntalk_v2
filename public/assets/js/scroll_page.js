7d4e739bd36d61d884d8b40100c80526c0daa582
준스 <junyeong@rocateer.com>
2024-11-04 15:34:40 +0900

    [사용자] scrollAct 함수 오류 수정

--- public/assets/js/scroll_page.js
+++ public/assets/js/scroll_page.js
@@ -79,30 +79,28 @@ function menuScroll() {
     const $menuList = $menu.find('div');
     const $menuItems = $menu.find('a')
     const $contents = $('.page');
-    const offsetMo = 50; // 메뉴 상단 고정 위치 (모바일)
-    const offsetPC = 0;  // 메뉴 상단 고정 위치 (PC)
+    //const offsetMo = 50;  메뉴 상단 고정 위치 (모바일)
+    //const offsetPC = 0;  메뉴 상단 고정 위치 (PC)
     const topMo = 80; // 스크롤 했을 때 컨텐츠 시작 위치 (모바일)
     const topPc = 150; // 스크롤 했을 때 컨텐츠 시작 위치 (PC)
     const breakpoints = 767; // 모바일 사이즈 분기점
     let windowWidth = window.innerWidth;
     let isMobile = window.innerWidth < breakpoints;
-    if ($menuWrap.length > 0) {
-        position = $menuWrap.offset().top;
-    }
+    let position = $menuWrap.offset().top;
     let resizeTimer;
     let scrollTimer;
 
     // 메뉴를 고정하는 함수
-    function scrollAct() {
-        const scrollTop = $(window).scrollTop();
-        const offset = isMobile ? offsetMo : offsetPC;
-        const menuWrapTop = isMobile ? offsetMo : offsetPC;
-        if (scrollTop > position - offset) {
-            $menuWrap.css({ 'position': 'fixed', 'top': menuWrapTop + 'px' });
-        } else {
-            $menuWrap.css({ 'position': 'absolute', 'top': '0' });
-        }
-    }
+    // function scrollAct() {
+    //     const scrollTop = $(window).scrollTop();
+    //     const offset = isMobile ? offsetMo : offsetPC;
+    //     const menuWrapTop = isMobile ? offsetMo : offsetPC;
+    //     if (scrollTop > position - offset) {
+    //         $menuWrap.css({ 'position': 'fixed', 'top': menuWrapTop + 'px' });
+    //     } else {
+    //         $menuWrap.css({ 'position': 'absolute', 'top': '0' });
+    //     }
+    // }
 
     // 활성화된 메뉴 항목을 중앙으로 이동하는 함수
     function activeMenu(target) {
