2a48e2ccdc529fadbf54510aa480c581431fe57d
준스 <junyeong@rocateer.com>
2024-11-29 21:58:07 +0900

    [사용자] 지식산업센터 평균가

--- resources/views/www/mypage/asset-detail.blade.php
+++ resources/views/www/mypage/asset-detail.blade.php
@@ -173,9 +173,9 @@
 
             if ($year < 1) {
                 $lastPrice = ($DPrice * 0.5) / 10000;
-            } elseif ($year == 1) {
+            } elseif ($year >= 1 && $year < 2) {
                 $lastPrice = ($DPrice * 0.6) / 10000;
-            } elseif ($year == 2) {
+            } elseif ($year >= 2 && $year < 3) {
                 $lastPrice = ($DPrice * 0.15) / 10000;
             } else {
                 $lastPrice = $DPrice * taxRate($DPrice);
