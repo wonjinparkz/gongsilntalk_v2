e7a995cc62bcfe18f6084ba1dd828bde593619b4
준스 <junyeong@rocateer.com>
2025-06-24 13:52:46 +0900

    [스케줄러] 확인 용

--- app/Console/Commands/GetAptInfo.php
+++ app/Console/Commands/GetAptInfo.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Http\Controllers\data\DataController;
+use App\Models\Notice;
 use Illuminate\Console\Command;
 
 class GetAptInfo extends Command
@@ -34,6 +35,11 @@ class GetAptInfo extends Command
         // $dataController->getAptPolygon();
         // $dataController->getAptCharacteristics();
         // $dataController->getAptuseWFS();
+
+        // 공지사항 viewcount 증가 예시
+        Notice::query()->increment('view_count');
+        // 또는 특정 공지사항만 증가시키려면:
+        // Notice::where('id', 1)->increment('view_count');
 
         $this->info('아파트 정보를 가져오는데 성공했습니다.');
     }
